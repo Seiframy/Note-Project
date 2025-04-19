8777b70a2052d3ecb156a6b94cecdbf3cd37249f
Seif Ramy <seif.ramy@ufe.edu.eg>
2025-04-19 19:46:17 +0200

    changed db connection

--- V4/php/dp.php
+++ V4/php/dp.php
@@ -10,14 +10,14 @@ ini_set('display_errors', 1);   // show errors
 // ini_set('display_errors', 0); // hide errors
 
 
-$host = "sqlXXX.infinityfree.com"; // use your real host name
-$dbname = "epiz_12345678_mydb";   // your real DB name
-$username = "epiz_12345678";      // your real username
+$host = "sql109.infinityfree.com"; // use your real host name
+$dbname = "if0_38781438_notes_app_db";   // your real DB name
+$username = "if0_38781438";      // your real username
 $password = "Blush12345";
 $port = 3306; // default MySQL port
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8;port=$port", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
